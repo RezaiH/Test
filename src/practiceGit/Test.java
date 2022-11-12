ff5adaf005608865d0ba9b4daf4350d9f0c05d0a
Hussain <hussain.rasa111@gmail.com>
2022-11-12 11:25:21 -0500

    final commit

--- src/practiceGit/Test.java
+++ src/practiceGit/Test.java
@@ -5,4 +5,11 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("I am out of work right now!");
 	}
+	
+	public static String showBack(String name, String lastName, int age) {
+		name = "Hussain";
+		lastName = "Rezai";
+		age = 34;
+		return name + " "+ lastName +" is" + age+ " years old";
+	}
 }
