730c79b6d83d2a40e06766ed2e91ad215e9b953f
Hussain <hussain.rasa111@gmail.com>
2022-11-12 11:37:57 -0500

    last commit for this scenario

--- src/practiceGit/CheckingRemoteRepo.java
+++ src/practiceGit/CheckingRemoteRepo.java
@@ -2,4 +2,15 @@ package practiceGit;
 
 public class CheckingRemoteRepo {
 
+	public static void main(String[] args) {
+		System.out.println("checking remote repository!");
+	}
+	
+	public static void sayHi() {
+		System.out.println("Hi, dude!, how are you?");
+	}
+	
+	public static void history() {
+		System.out.println("You are the best bro!");
+	}
 }
